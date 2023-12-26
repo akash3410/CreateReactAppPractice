87e26d3da69d5cc45658c25ce45b74a3eee6d0d1
akash3410 <mdarifuzzaman982k@gmail.com>
2023-12-26 20:53:34 +0600

    Componenet Tree (JSX/without JSX)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <Person />
+      {/* or */}
+      {/* <Person></Person> */}
     </div>
   );
+
+  // Another Way
+  // return React.createElement('div', { className: "App" }, React.createElement('h1', null, "Hello World!"), <Person />)
+}
+function Person() {
+  return (
+    <div>
+      <h1>I am a Person</h1>
+    </div>
+  );
+
+  // Another Way
+  // return React.createElement('div', null, React.createElement('h1', null, "I am Person!"))
 }
 
 export default App;
